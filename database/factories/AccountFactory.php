0ac1ea2178b1b012b42f830699ef9f4fb3caa8fe
Hecton <hecton@localhost.localdomain>
2019-06-06 14:55:03 -0300

    First

---
+++ database/factories/AccountFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Account;
+use Faker\Generator as Faker;
+
+$factory->define(Account::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
